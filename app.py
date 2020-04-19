61950f77469567f3f9c7c99de9dabf8dd49db359
Keaton Brown <keaton.brown@colorado.edu>
2020-04-19 16:11:53 -0600

    Added analysis

--- app.py
+++ app.py
@@ -10,12 +10,29 @@ import pymongo
 app = Flask(__name__, template_folder='templates')
 
 # Define app routes
+# On page load
 @app.route('/', methods=['GET'])
 def index():
     data = get_tickers.scrape()
     print(data)
     # print(stockDict)
     return render_template("index.html", data=data)
+# Home route
+@app.route('/index.html')
+def home():
+    data = get_tickers.scrape()
+    print(data)
+    # print(stockDict)
+    return render_template("index.html", data=data)
+# Sources route
+@app.route('/sources.html')
+def sources():
+    return render_template("sources.html")
+# Analysis route
+@app.route('/analysis.html')
+def analysis():
+    data = get_tickers.scrape()
+    return render_template("analysis.html", data=data)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/stock_data.js
+++ static/js/stock_data.js
@@ -1,8 +1,5 @@
-// The purpose of this script is to retrieve the value of the selected
-// ticker on from the HTML, and grab the data and generate charts once
-// the 'Analyze' button is clicked.
+// Run the getData function when the Visualize button is clicked
 
-// Run the getData function when the button is clicked
 function getData() {
     dropdownMenu = d3.select("select");
     var ticker = dropdownMenu.property("value");
@@ -15,6 +12,11 @@ function getData() {
     // Call the Alphavantage API to retreive stock data
     d3.json(queryURL, function(data) {
         console.log(data);
+
+        // Throw an alert if the user selects a ticker that isn't on Quandl
+        window.onerror = function(message) {
+            this.alert(`Oops! Quandl wasn't able to find ${ticker}. Please select another ticker.`)
+        };
 
         function unpack(rows, index) {
             return rows.map(function(row) {
@@ -33,15 +35,25 @@ function getData() {
         var closingPrices = unpack(data.dataset.data, 4);
         var volume = unpack(data.dataset.data, 5);
 
+        // Calculate the percent change
         var lastClose = closingPrices[0];
         var firstClose = closingPrices[closingPrices.length - 1];
         var change = ((lastClose - firstClose) / firstClose) * 100;
+
+        // Calculate the average daily volume
+        var total = 0;
+        for (var i = 0; i < volume.length; i++) {
+            total += volume[i];
+        }
+
+        var avgDailyVolume = total / volume.length;
         
         // Add in some text information about the selected stock
         document.getElementById("company").textContent = `${name}`;
         document.getElementById("periodHigh").textContent = `Period High: $${Math.max.apply(Math, high)}`;
         document.getElementById("periodLow").textContent = `Period Low: $${Math.min.apply(Math, low)}`;
         document.getElementById("percentChange").textContent = `Change in Period: ${change.toFixed(2)}%`;
+        document.getElementById("avgVolume").textContent = `Avg Volume: ${Math.round(avgDailyVolume)}`;
 
         // Create a candlestick chart
         var trace1 = {
@@ -92,4 +104,56 @@ function getData() {
 
     });
 
+};
+
+// Run the getMovingAvgData function when the Analyze button is clicked
+
+function getMovingAvgData() {
+
+    // Clear old chart if one exists
+    document.getElementById("movingAvgChart").innerHTML = "";
+
+    dropdownMenu = d3.select("select");
+    var ticker = dropdownMenu.property("value");
+    console.log(ticker);
+
+    // Create URL with selected ticker symbol
+    var queryURL = `https://www.quandl.com/api/v3/datasets/WIKI/${ticker}.json?start_date=2017-01-01&end_date=2018-01-01&api_key=${apikey}`
+    console.log(queryURL);
+
+    // Call the Alphavantage API to retreive stock data
+    d3.json(queryURL, function(data) {
+        console.log(data);
+
+        // Throw an alert if the user selects a ticker that isn't on Quandl
+        window.onerror = function(message) {
+            this.alert(`Oops! Quandl wasn't able to find ${ticker}. Please select another ticker.`)
+        };
+    
+        // Use AnyChart to create display moving averages
+        // Create an empty datatable and add data
+        var dataTable = anychart.data.table(0);
+        dataTable.addData(data.dataset.data)
+        // Map columns in datatable
+        var mapping = dataTable.mapAs({
+            value: 4
+        });
+
+        var mapping2 = dataTable.mapAs({
+            open: 1,
+            high: 2,
+            low: 3,
+            close: 4
+        });
+
+        // Create a stock chart
+        var chart = anychart.stock();
+        chart.plot(0).sma(mapping, 20).series();
+        chart.plot(0).sma(mapping, 50).series();
+        chart.plot(0).ohlc(mapping2).name("Price");
+        chart.title(`${ticker} 20 and 50-Day SMAs`);
+        chart.container('movingAvgChart');
+        chart.draw();
+
+    });
 };
